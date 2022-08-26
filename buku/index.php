7acdf2398bfe51745d8775fa2180e8a50bdf0bdb
unknown <berlian@aknpacitan.ac.id>
2022-08-26 14:42:32 +0700

    Progress

--- buku/index.php
+++ buku/index.php
@@ -7,6 +7,27 @@
 
     <h3>Master Buku</h3>
     <a href="formAdd.php">Tambah</a>
+    <table>
+  <thead>
+    <tr>
+      <th>No</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+  </thead>
+  <tbody>
 
+ <?php //while($buku = $hasil->fetch_assoc()) { ?>
+         <tr>
+         <th><?php //echo $i; $i++; ?></th>
+         <td> <?php //echo $buku['jur_nama']; ?></td>
+         <td>
+         <a href="" class="btn btn-warning">Edit</a>|
+          <a href="" class="btn btn-danger">Delete</a> 
+         </td>
+    </tr>                                                        <!--baru-->
+<?php //} ?>
+  </tbody>
+</table>
 </body>
 </html>
