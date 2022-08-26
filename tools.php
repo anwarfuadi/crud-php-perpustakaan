33b4f40ed02594ba7d8afe488563e89009e659d5
unknown <berlian@aknpacitan.ac.id>
2022-08-26 15:05:24 +0700

    tambah function

--- tools.php
+++ tools.php
@@ -2,18 +2,30 @@
 //Include CONFIG
 include('config.php');
 //fungsi INSERT
-
+function insert() {
+    //code to be executed;
+  }
 
 //fungsi SELECT
-
+function select() {
+   // code to be executed;
+  }
 
 //fungsi UPDATE
-
+function update() {
+   // code to be executed;
+  }
 
 //fungsi DELETE
-
+function delete() {
+    //code to be executed;
+  }
 
 //fungsi SEARCH
-
+function search() {
+    //code to be executed;
+  }
 
 //fungsi SORT
+
+$con->close();
