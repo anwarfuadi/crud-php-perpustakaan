725edf1b5ca886dfce62ff3c685a033400b53ff5
SUGAB <bagushikmahwan@gmail.com>
2022-08-26 14:13:01 +0700

    Sugab

--- config.php
+++ config.php
@@ -1,1 +1,10 @@
 <?php
+$conn = new mysqli("localhost", "root", "", "perpustakaan");
+
+if ($conn->connect_errno) {
+
+    echo die("Failed to connect to MySQL: " . $conn->connect_error);
+
+}
+
+?>
