9934417962c44e58b861910b8554a376f775bb18
SUGAB <bagushikmahwan@gmail.com>
2022-08-26 15:07:15 +0700

    nambah config diheader

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+<?php include('config.php') ?>
+
 <a href="/crud-php-perpustakaan">Beranda</a> ||
 <a href="/crud-php-perpustakaan/kategori">Kategori Buku</a> ||
 <a href="/crud-php-perpustakaan/buku">Buku</a>
