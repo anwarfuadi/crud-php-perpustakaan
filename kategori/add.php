22c6c9d0f6693bdc110c2b0e905b54ff05dda6a5
SUGAB <bagushikmahwan@gmail.com>
2022-08-27 01:39:01 +0700

    update gagal...ampun

--- kategori/add.php
+++ kategori/add.php
@@ -1,1 +1,15 @@
 <?php
+include('../tools.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $kode=$_POST["kode"];
+    $kategori=$_POST["kategori"];
+    
+    $sql = "INSERT INTO kategori_buku(kode_kategori,kategori_buku) 
+    VALUES ('$kode','$kategori')";
+    
+    insertkategori($sql);
+}
+
+?>
--- kategori/formEdit.php
+++ kategori/formEdit.php
@@ -3,9 +3,28 @@
     <title>Perpustakaan - Master Kategori</title>
 </head>
 <body>
-    <?php include('../header.php') ?>
+<?php   include '../header.php';
+        include '../tools.php';
+        
+$id_kategori = $_POST['id_kategori'];
 
-    <h3>Edit Data Master Kategori</h3>
+$sql_kategori = "select * from kategori_buku where id_kategori='$id_kategori'";
 
+$hasil_kategori = select($sql_kategori);
+$data = $hasil_kategori->fetch_assoc();
+
+?>
+
+    <h3>Edit Data Master Kategori Buku</h3>
+    <form action="edit.php" method="post">
+    <label>Id kategori</label>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="number" value="<?php echo $id_kategori; ?>" name="id_kategori" disabled><br>
+    <label>Kode Kategori</label>
+    <input type="text" value="<?php echo $data['kode_kategori']; ?>" name="kode" required><br>
+    <label>Kategori Buku</label>
+    <input type="text" value="<?php echo $data['kategori_buku']; ?>" name="kategori" required><br>
+    <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
